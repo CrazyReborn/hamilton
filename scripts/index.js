739a8cfd823db84dd159225d2568aee3ee15f567
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-07-04 09:12:50 +0200

    there is an algorithm selector in the navbar.

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,19 @@ import { selectionSortOnNodeList } from './algorithms/selectionSort.js';
 (function main() {
   const rangeInputElement = document.getElementById('range');
   const startButton = document.getElementById('start');
+  const algoSelector = document.getElementById('select-algo');
+  let selectedAlgo = '';
   let rangeInputValue = 0;
+
+  algoSelector.addEventListener('change', (e) => {
+    switch (e.target.value) {
+      case '':
+        break;
+      case 'selection-sort':
+        selectedAlgo = 'selection-sort';
+        break;
+    }
+  })
 
   rangeInputElement.addEventListener('change',(e) => {
     rangeInputValue = e.currentTarget.value;
@@ -13,7 +25,10 @@ import { selectionSortOnNodeList } from './algorithms/selectionSort.js';
 
   startButton.addEventListener('click', () => {
     const nodeList = document.getElementsByClassName('array-element');
-    selectionSortOnNodeList(nodeList);
+    switch(selectedAlgo) {
+      case 'selection-sort':
+        selectionSortOnNodeList(nodeList);
+    }
   })
 })();
 
